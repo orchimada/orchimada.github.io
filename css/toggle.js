cbc47e9ad81290cb05644dae522e638ba5463d68
Orchimada <art.lyarsky@gmail.com>
2017-09-12 16:15:15 +0700

    js

---
+++ css/toggle.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var books = document.getElementById('book_list');
+    if (books.style.display === 'none') {
+        books.style.display = 'block';
+    } else {
+        books.style.display = 'none';
+    }
+}
