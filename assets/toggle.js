1eee010e17b7c43656700abe8fddc0f05f5fadbd
Orchimada <art.lyarsky@gmail.com>
2017-09-12 19:17:10 +0700

    fx

--- assets/toggle.js
+++ assets/toggle.js
@@ -2,15 +2,7 @@ $( document ).ready(function() {
     console.log( "ready!" );
 });
 
-$( '#books_switch' ).onclick(function() {
+$( '#books_switch' ).click(function() {
     console.log( "found" );
 });
 
-function myFunction() {
-    var books = document.getElementById('book_list');
-    if (books.style.display === 'none') {
-        books.style.display = 'block';
-    } else {
-        books.style.display = 'none';
-    }
-}
