d494aaa37933a2796f00edf183c676e68baf4ddc
Orchimada <art.lyarsky@gmail.com>
2017-09-12 21:06:09 +0700

    toggler

--- assets/toggle.js
+++ assets/toggle.js
@@ -4,5 +4,11 @@ $( document ).ready(function() {
 
 $(document).on("click", '#books_switch', function() {
     console.log( "found" );
+    var books = $('#book_list');
+    if (books.style.display === 'none') {
+        books.style.display = 'block';
+    } else {
+        books.style.display = 'none';
+    }
 });
 
