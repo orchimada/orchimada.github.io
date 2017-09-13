4a021107dcb9ed73646a947f2a2db7bd55c7d692
Orchimada <art.lyarsky@gmail.com>
2017-09-13 14:09:19 +0700

    class

--- assets/toggle.js
+++ assets/toggle.js
@@ -4,6 +4,6 @@ $( document ).ready(function() {
 
 $(document).on("click", '#books_switch', function() {
     console.log( "found" );
-    $('#book_list').classList.toggle('menuStyle');
+    $('#book_list').toggleClass('menuStyle');
 });
 
